faf1b669b36f925b54e09e3c734ece549a2d9fbb
ryang <ryan.r.c.339ac@gmail.com>
2021-09-30 20:33:28 -0300

    created a slider and made it work and also imported fontawesome

---
+++ javascript/slider.js
@@ -0,0 +1,38 @@
+const carouselSlide = document.querySelector(".carousel-container__carousel-slide")
+const carouselImages = document.querySelectorAll(".carousel-slide__carousel-items")
+console.log(carouselImages[0])
+//btn
+const prevBtn = document.querySelector('#prevBtn')
+const nextBtn = document.querySelector('#nextBtn')
+
+counter = 1
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = `translateX(${-size * counter}px)`
+//btn listeners
+prevBtn.addEventListener('click', () =>{
+    if(counter <= 0 ) return
+    carouselSlide.style.transition = "transform 0.5s ease-in-out"
+    counter --
+    carouselSlide.style.transform = `translateX(${-size * counter}px`
+})
+nextBtn.addEventListener('click', () =>{
+    if(counter >= carouselImages.length -1) return
+    carouselSlide.style.transition = 'transform 0.5s ease-in-out'
+    counter ++
+    carouselSlide.style.transform = `translateX(${-size * counter}px`
+})
+
+carouselSlide.addEventListener('transitionend', ()=> {
+    if(carouselImages[counter].id === 'last-clone'){
+        carouselSlide.style.transition = "none"
+        counter = carouselImages.length -2
+        carouselSlide.style.transform = `translateX(${-size * counter}px`
+    }
+    if(carouselImages[counter].id === 'fist-clone'){
+        carouselSlide.style.transition = "none"
+        counter = carouselImages.length - counter
+        carouselSlide.style.transform = `translateX(${-size * counter}px`
+    }
+})
+
