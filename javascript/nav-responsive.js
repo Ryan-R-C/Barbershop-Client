ef37ea5c3a6bc44e68747224c885d4950c7db861
ryang <ryan.r.c.339ac@gmail.com>
2021-10-01 21:03:52 -0300

    removed unsefull files and created a new nav-bar to mobile

---
+++ javascript/nav-responsive.js
@@ -0,0 +1,20 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const nav = document.querySelector(".responsive-navbar__nav-list")
+    const liLinks = document.querySelectorAll("responsive-navbar__nav-list__list-el")
+    
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-list--active')
+        //Animate Links    
+        liLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ``
+            }
+            else{
+                link.style.animation = `nav-link--fade 0.5s ease fowards ${index/ 7 + 1.5}s`
+            }
+            })
+    })
+}
+navSlide(); 
