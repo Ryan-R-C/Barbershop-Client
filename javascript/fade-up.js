c15bc73884cacb993006b9e67d8857f07b85e823
ryang <ryan.r.c.339ac@gmail.com>
2021-10-03 16:40:10 -0300

    created the fade up animation to the elements

---
+++ javascript/fade-up.js
@@ -0,0 +1,13 @@
+const introContent = document.querySelectorAll(".intro-disabled")
+console.log(introContent)
+window.addEventListener('scroll', ()=>{
+    
+    introContent.forEach(introContent => {
+            let introPositon = introContent.getBoundingClientRect().top
+            let screenPosition = window.innerHeight/1.2
+
+            if(introPositon < screenPosition){
+                introContent.classList.add('intro-active')
+            }
+        });
+    })
